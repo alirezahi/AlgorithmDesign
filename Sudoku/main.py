d43199c8c02971c52695c2ef3f6705a6ab695fd6
Alireza <alireza97hi@gmail.com>
2017-05-14 18:31:17 +0430

    Sudoku Solver added

--- Sudoku/main.py
+++ Sudoku/main.py
@@ -1,3 +1,8 @@
+def print_table(table):
+    for i in range(9):
+        for j in range(9):
+            print(table[i][j],end=' ')
+        print()
 def find_zero(table):
     for i in range(9):
         for j in range(9):
@@ -5,13 +10,48 @@ def find_zero(table):
                 return i,j
     return -1,-1
 
+def row_okay(test_num,row,col,table):
+    for i in range(9):
+        if i!=col and test_num == table[row][i]:
+            return False
+    return True
+
+def col_okay(test_num,row,col,table):
+    for i in range(9):
+        if i!=row and test_num == table[i][col]:
+            return False
+    return True
+
+def round_okay(test_num,row,col,table):
+    for i in range(int(row/3)*3,int(row/3)*3+3):
+        for j in range(int(col/3) * 3, int(col/3) * 3 + 3):
+            if not(i==row and j==col) and test_num==table[i][j]:
+                return False
+    return True
+
+def ok(test_num,row,col,table):
+    if col_okay(test_num,row,col,table) and row_okay(test_num,row,col,table) and round_okay(test_num,row,col,table):
+        return True
+    return False
+
 def solve(table):
-    return 0
+    i,j = find_zero(table)
+    if i+j == -2:
+        print_table(table)
+        return True
+    for z in range(1,10):
+        if ok(z,i,j,table):
+            table[i][j]=z
+            if not solve(table):
+                table[i][j]=0
+            else:
+                return True
+    return False
 
 table = [[0 for i in range(9)] for j in range(9)]
 for i in range(9):
     line = input().split(' ')
     for j in range(9):
-        table[i][j] = line[j]
+        table[i][j] = int(line[j])
 
-table = solve(table)
+solve(table)
